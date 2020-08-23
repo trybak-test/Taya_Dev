861ec8f4ebc752edea3785ba7940e0f0bbe2fed4
taya_rybak <trybak@deloitte.nl>
2020-08-23 22:13:00 +0200

    init

---
+++ test_gouogle.py
@@ -0,0 +1,39 @@
+
+from selenium import webdriver
+import unittest
+import HtmlTestRunner
+from selenium.webdriver.common.keys import Keys
+import time
+
+class GoogleSearch(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        print('Start test')
+        cls.driver = webdriver.Chrome("C:\Temp\chromedriver.exe")
+        cls.driver.get('https://google.com')
+        time.sleep(2)
+
+
+    def test_01(self):
+        print('Python search')
+        driver=self.driver
+        input_field = driver.find_element_by_name('q')
+        input_field.send_keys('python')
+        input_field.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+    def test_02(self):
+        print('Test Automation search')
+        driver=self.driver
+        input_field = driver.find_element_by_name('q')
+        input_field.send_keys('test automation')
+        input_field.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        print('Test passed')
+
+if __name__=='__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/Users/trybak/PycharmProjects/TrainingDev/Report'))
