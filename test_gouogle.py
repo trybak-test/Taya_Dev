b7fd7f89f206eccc9532623e99d940311ede5489
taya_rybak <trybak@deloitte.nl>
2020-08-23 22:43:08 +0200

    change path to chromedriver

--- test_gouogle.py
+++ test_gouogle.py
@@ -10,7 +10,9 @@ class GoogleSearch(unittest.TestCase):
     @classmethod
     def setUp(cls):
         print('Start test')
-        chromeDriverPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "SeleniumDrivers\chromedriver.exe")
+        chromeDriverPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "SeleniumDrivers", "chromedriver")
+        if os.name == 'nt':
+            chromeDriverPath += ".exe"
         cls.driver = webdriver.Chrome(chromeDriverPath)
         cls.driver.get('https://google.com')
         time.sleep(2)
